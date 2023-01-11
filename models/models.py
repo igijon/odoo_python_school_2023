5998a86a49d3ae48328e0b2d1f3c650b6be57f3d
Inma Gijón <igijoninf@gmail.com>
2023-01-11 17:13:54 +0100

    Fields básicos. Campos obligatorios

--- models/models.py
+++ models/models.py
@@ -7,7 +7,7 @@ class student(models.Model):
     _name = 'school.student'
     _description = 'school.student'
 
-    name = fields.Char()
+    name = fields.Char(string="Nombre", readonly=False, required=True, help="Este es el nombre")
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
