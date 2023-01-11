5058fc483b0e4bf22e08255b5428057ee8132542
Inma Gijón <igijoninf@gmail.com>
2023-01-11 17:19:19 +0100

    Fields básicos

--- models/models.py
+++ models/models.py
@@ -8,11 +8,9 @@ class student(models.Model):
     _description = 'school.student'
 
     name = fields.Char(string="Nombre", readonly=False, required=True, help="Este es el nombre")
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    birth_year = fields.Integer()
+    description = fields.Text()
+    inscription_date = fields.Date()
+    last_login = fields.Datetime()
+    is_student = fields.Boolean()
+    photo = fields.Binary()
